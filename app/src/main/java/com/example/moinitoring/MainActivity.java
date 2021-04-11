bb4d12fe8e91be624fb74b6054bd7df69f597aff
Young-Jang <b116104@naver.com>
2021-04-11 23:38:12 +0900

    SharedPreferences에 JSON으로 저장

--- app/src/main/java/com/example/moinitoring/MainActivity.java
+++ app/src/main/java/com/example/moinitoring/MainActivity.java
@@ -7,20 +7,28 @@ import androidx.core.content.ContextCompat;
 import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.telephony.SmsMessage;
 import android.util.Log;
 import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
 
-    private static ArrayList<SMSData> SMSDataList = new ArrayList<SMSData>();
+    private static ArrayList<SMSData> SMSDataList;
     private static final String TAG = "MainActivity";
-
+    private static final String SETTINGS_PLAYER_JSON = "settings_item_json";
+    ListView listView;
+    SMSAdapter myAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,13 +36,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         requirePerms();
 
+        Log.d(TAG,"start");
+        SMSDataList = new ArrayList<SMSData>();
+
         Intent intent = getIntent();
         processCommand(intent);
-
-        ListView listView = (ListView)findViewById(R.id.listView);
-        final SMSAdapter myAdapter = new SMSAdapter(this,SMSDataList);
-
-        listView.setAdapter(myAdapter);
     }
 
     @Override
@@ -44,7 +50,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void processCommand(Intent intent){
-
+        SMSDataList.clear();
+        getStringArrayPref(this,SETTINGS_PLAYER_JSON);
         if(intent != null){
             String sender = intent.getStringExtra("sender");
             String date = intent.getStringExtra("date");
@@ -54,7 +61,70 @@ public class MainActivity extends AppCompatActivity {
             Log.d(TAG,"content: "+content);
 
             SMSDataList.add(new SMSData(date,sender, content,R.drawable.ic_launcher_background));
+            setStringArrayPref(this,SETTINGS_PLAYER_JSON,SMSDataList);
         }
+    }
+
+    private void setStringArrayPref(Context context, String key, ArrayList<SMSData> values) {
+        Log.d(TAG,"Json start");
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor editor = prefs.edit();
+        JSONArray a = new JSONArray();
+
+        for(int i =0;i<values.size();i++) {
+
+            JSONObject jsonObject = new JSONObject();
+            try {
+                Log.d(TAG,"setStringJson start!"+i);
+                Log.d(TAG,"sender: "+values.get(i).getSendNumber());
+                Log.d(TAG,"date: "+values.get(i).getDate());
+                Log.d(TAG,"content: "+values.get(i).getContent());
+                jsonObject.put("sender", values.get(i).getSendNumber());
+                jsonObject.put("date", values.get(i).getDate());
+                jsonObject.put("content", values.get(i).getContent());
+                jsonObject.put("warning", values.get(i).getWarning());
+            }catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+            a.put(jsonObject);
+        }
+//        for (int i = 0; i < values.size(); i++) {
+//            a.put(values.get(i));
+//        }
+        if (!values.isEmpty()) {
+            editor.putString(key, a.toString());
+        } else {
+            editor.putString(key, null);
+        }
+        Log.d(TAG,"toJson: "+a.toString());
+        editor.apply();
+    }
+
+    private void getStringArrayPref(Context context, String key) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        String json = prefs.getString(key, null);
+        ArrayList<SMSData> urls = new ArrayList<SMSData>();
+        if (json != null) {
+            try {
+                JSONArray a = new JSONArray(json);
+                for (int i = 0; i < a.length(); i++) {
+                    JSONObject url = a.getJSONObject(i);
+
+                    Log.d(TAG,"getStringJson start!");
+                    Log.d(TAG,"sender: "+url.getString("sender"));
+                    Log.d(TAG,"date: "+url.getString("date"));
+                    Log.d(TAG,"content: "+url.getString("content"));
+                    SMSDataList.add(new SMSData(url.getString("date"),url.getString("sender"),url.getString("content"),url.getInt("warning")));
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        listView = (ListView)findViewById(R.id.listView);
+        myAdapter = new SMSAdapter(this,SMSDataList);
+
+        listView.setAdapter(myAdapter);
     }
 
     private void requirePerms(){
