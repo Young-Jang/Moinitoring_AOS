56c6cd905985cee556cf9f0175faeba3745eab3d
Young-Jang <b116104@naver.com>
2021-04-11 14:54:19 +0900

    Initial commit

---
+++ app/src/main/java/com/example/moinitoring/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.moinitoring;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        requirePerms();
+    }
+
+    private void requirePerms(){
+        String[] permissions = {Manifest.permission.RECEIVE_SMS};
+        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_SMS);
+        if(permissionCheck == PackageManager.PERMISSION_DENIED){
+            ActivityCompat.requestPermissions(this, permissions, 1);
+        }
+    }
+}
